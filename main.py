da43ba85f01f254ec89dd1eee23586e76d3c2529
sebbiegithub <slambert.cork@gmail.com>
2024-01-16 17:04:10 +0000

    Added some sensor values

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 def on_button_pressed_a():
     global temp
     temp = input.temperature()
-    print("Temp:{0}".format(temp))
+    print("Temp:"+temp)
     basic.show_number(temp)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    global mylight
+    print("Light:"+light)
+    mylight = input.light_level()
+    basic.show_number(mylight)
+
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 temp = 0
+mylight = 0
 input.temperature()
+input.light_level()
+
--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
+
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
     temp = input.temperature()
-    console.log("Temp:{}")
+    date = timeanddate.dateTime()
+    console.log(date + " Temp: " + temp)
     basic.showNumber(temp)
 })
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    mylight = input.lightLevel()
+    date = timeanddate.dateTime()
+    console.log(date + " Light: " + mylight)
+    basic.showNumber(mylight)
+})
 let temp = 0
+let mylight = 0
+let date = ""
 input.temperature()
+input.lightLevel()
